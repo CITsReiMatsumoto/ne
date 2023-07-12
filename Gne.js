523c98612728ab758834d86efa23dde7e3488eb9
CITsReiMatsumoto <131419193+CITsReiMatsumoto@users.noreply.github.com>
2023-07-12 23:12:43 +0900

    Update Gne.js

--- Gne.js
+++ Gne.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const app = express();
 
-//データベース交信
+//データベース初期化
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('dataNE');       //dataNE = 作成データ
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
 
 //データGneの取得
 app.get("/db", (req, res) => {
@@ -18,12 +21,25 @@ app.get("/db", (req, res) => {
     })
 })
 
-
+//取得データの出力
+app.get("/top", (req, res) => {
+    let desc = "";
+    if( req.query.desc ) desc = " desc";
+    let sql = "select name, kind, mw, ld from Gne" + req.query.pop + ";";
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('show', {mes:"EROOR"});
+            }
+            res.render('select', {data:data});
+        })
+    })
+})
 
 //未発見時の表記
 app.use(function(req, res, next) {
   res.status(404).send('No page');
 });
 
-//終了
+//終
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
